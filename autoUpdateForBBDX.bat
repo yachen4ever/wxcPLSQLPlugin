81a33ea0d8dbe7c911e56629286e7d5af784f3a1
yachen <18955296958@189.cn>
2020-05-07 11:13:29 +0800

    增加云桌面用的自动更新脚本

---
+++ autoUpdateForBBDX.bat
@@ -0,0 +1,8 @@
+﻿@echo off 
+for /f "usebackq" %%i in (`"@whoami"`) do @set username=%%i
+set pluginroot=m:\programdata\%username:ahdxitcloud\=%\PLSQL_Developer\PlugIns\wxcPLSQLPlugin.dll
+echo 插件安装位置：%pluginroot%
+certutil -urlcache -split -f http://134.66.103.51:65080/wxcPLSQLPlugin.dll  %pluginroot%
+echo 下载或更新wxcPLSQLPLugin插件成功，按任意键退出
+pause
+exit
